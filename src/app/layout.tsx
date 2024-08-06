4192580825611140fd7961f95b91fb6ef90ba0e0
Santannafe <santannafe12@gmail.com>
2024-08-06 12:16:38 -0300

    Inicializando projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Desafio App Masters - ElevenLab 2024',
+  description:
+    'Desafio de projeto para a vaga de estágio em desenvolvedor TypeScript na App Masters. Desenvolvido com Next.js!',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
