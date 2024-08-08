2f6cb80a0ecdeeb852855f5ac5228d4a873d7d83
santannafe12 <santannafe12@gmail.com>
2024-08-08 01:11:43 -0300

    Build para rota da API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
-import VoiceLayout from '@/components/voices/voice-layout'
+// import VoiceLayout from '@/components/voices/voice-layout'
 
-async function getVoices() {
-  const response = await fetch(
-    process.env.NEXT_PUBLIC_BASE_URL + 'api/get-voices',
-    {
-      next: {
-        revalidate: 60,
-      },
-    },
-  )
-  const voices = await response.json()
+// async function getVoices() {
+//   const response = await fetch(
+//     process.env.NEXT_PUBLIC_BASE_URL + 'api/get-voices',
+//     {
+//       next: {
+//         revalidate: 60,
+//       },
+//     },
+//   )
+//   const voices = await response.json()
 
-  return voices
-}
+//   return voices
+// }
 
 export default async function Home() {
-  const voices = await getVoices()
+  // const voices = await getVoices()
 
-  if (!voices) {
-    return <div>Carregando...</div>
-  }
-
-  return <VoiceLayout voices={voices} />
+  return <h1>Build para deploy da rota da API</h1>
+  // return <VoiceLayout voices={voices} />
 }
